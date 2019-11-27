1081d9036fc982b11e4f16ec008f9334e8540a56
Mithu Mondal <mithu2649@gmail.com>
2019-11-27 18:21:49 +0530

    Add files via upload

--- YoutubeToMp3.py
+++ YoutubeToMp3.py
@@ -6,26 +6,42 @@ from pytube import YouTube
 import os
 
 word_list = []
-print("What song do you want to listen to?: \n")
+print("Song name - artist  OR Youtube URL\n")
 songToSearch = input()
+
+#searh YouTube for the video/song
 query = urllib.parse.quote(songToSearch)
 url = "https://www.youtube.com/results?search_query=" + query
+
+#get the response and find all the video id (with class yt-uix-title-link)
+#and store them in the word_list[]
 response = urllib.request.urlopen(url)
 html = response.read()
 soup = BeautifulSoup(html, 'html.parser')
 for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
     word_list.append(vid['href'])
 
+#get the first video link
 songURL = "https://youtube.com"+word_list[0]
 
 yt = YouTube(songURL)
-songToDownload = yt.streams.get_by_itag('140')
+songToDownload = yt.streams.get_by_itag('140') #stream itag 140 is for mp4 audio
+
 songName = yt.title
 fileSize = songToDownload.filesize
+
+
+#download the file
 print("downloading.. ["+ str(fileSize/(1024*0.00000095367432)) +"MB]: "+songName)
-
 songToDownload.download()
+print("Download Complete.")
 
-#rename the mp4 audio file to mp3
-#works for most of the systems with no conversion
-os.rename("/tmp/"+songName+".mp4", "/tmp/"+songName+".mp3")
+try:
+    #rename the mp4 audio file to mp3
+    #works for most of the systems with no conversion
+    os.rename("/tmp/"+songName+".mp4", "/tmp/"+songName+".mp3")
+    print("Converted Succesfully")
+except:
+    print("Something went wrong. \nPlease convert the file manually using other software.")
+
+
